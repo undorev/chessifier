6edeee26707ab15bd9a3c9bc6b0c2950da46f337
Rustam Abrahamyan <rustam.wd@gmail.com>
2025-08-07 17:50:43 +0400

    fix: window dragging and minimize action

--- src/common/components/TopBar.css.ts
+++ src/common/components/TopBar.css.ts
@@ -1,9 +1,8 @@
 import { style } from "@vanilla-extract/css";
-import { vars } from "@/styles/theme";
 
 export const icon = style({
   transition: "background-color 100ms ease",
   ":hover": {
-    backgroundColor: vars.colors.dark[5],
+      backgroundColor: 'light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-5))',
   },
 });
